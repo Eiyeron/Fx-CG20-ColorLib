f7da06d4ed8db3dbbd850d57295e988797c995ab
Eiyeron FULMICENDII <eiyeron@gmail.com>
2013-10-12 18:36:44 +0200

    Started cleaning Lib

--- ColorLib.h
+++ ColorLib.h
@@ -32,5 +32,58 @@
 // #define INT2STR
 // #define FPS
 
+
+/**************************/
+/** Useful Constants     **/
+/**************************/
+
+#define LCD_WIDTH_PX 384
+#define LCD_HEIGHT_PX 216
+#define VRAM 0xA8000000
+
+/**************************/
+/** Structures           **/
+/**************************/
+
+typedef struct {
+    char* data;
+    int width, height, flags;
+} Font;
+
+/**************************/
+/** Flags                **/
+/**************************/
+// Font flags :
+#define MONOSPACE    0x00000001
+#define ANTIALIASING 0x00000002
+
+
+/**************************/
+/** Functions prototypes **/
+/**************************/
+
+int key_down(int basic_keycode);
+
+void line(int x1, int y1, int x2, int y2, short color);
+
+void point(int x, int y, int color); 
+
+void text_setFont(Font* font);
+
+Font* text_getActualFont();
+
+void text_printC(int x, int y, char c, int size, int color);
+
+void text_print(int x, int y, char* c, int size, int color);
+
+int text_widthC(char c);
+
+int text_width(char* c);
+
+void alphaSprite(int x, int y, int width, int height, short* bitmap, short alpha);
+
+void CopySpriteMasked(short* bitmap, int x, int y, int width, int height, short mask);
+
+void CopySpriteNbitMasked(const unsigned char* data, int x, int y, int width, int height, const short* palette, short maskColor, unsigned int bitwidth);
 #endif // COLORLIB
 
